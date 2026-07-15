a5cfdc373693989adae5ad6341058f6c0c57b251
Ali Ahmed <aa200631234@gmail.com>
2026-07-15 16:31:57 +0300

    Update env.py

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -8,10 +8,6 @@ from app.config import settings
 from app.data.models import Base
 
 config = context.config
-config.set_main_option(
-    "sqlalchemy.url",
-    settings.database_url.replace("%", "%%")
-)
 
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
